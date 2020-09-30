7934ec3acc59aedb43aaeaed14de03193ce0904e
DivinaThomas <divina180@gmail.com>
2020-09-30 01:17:10 +0100

    Defining GPU properties

---
+++ gpu.py
@@ -0,0 +1,12 @@
+from google.appengine.ext import ndb
+
+# Defining GPU properties
+class Gpu(ndb.Model):
+	geometryShader = ndb.StringProperty()
+	tesselationShader = ndb.StringProperty()
+	shaderInt16 = ndb.StringProperty()
+	sparseBinding = ndb.StringProperty()
+	textureCompressionETC2 = ndb.StringProperty()
+	vertexPipelineStoresAndAtomics = ndb.StringProperty()
+
+
